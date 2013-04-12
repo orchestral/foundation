fa46bac3bfbcc38c0d96fb5454a977735e7e6c47
crynobone <crynobone@gmail.com>
2013-04-12 20:10:09 +0800

    Add Orchestra\Foundation\Model\UserMeta
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Orchestra/Foundation/Model/UserMeta.php
@@ -0,0 +1,25 @@
+<?php namespace Orchestra\Foundation\Model;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class UserMeta extends Eloquent {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'user_meta';
+
+	/**
+	 * Belongs to relationship with User.
+	 *
+	 * @access public
+	 * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	public function users()
+	{
+		return $this->belongsTo('\Orchestra\Foundation\Model\User', 'user_id');
+	}
+
+}
---
+++ tests/Model/UserMetaTest.php
@@ -0,0 +1,47 @@
+<?php namespace Orchestra\Foundation\Tests\Model;
+
+class UserMetaTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * Set mock connection
+	 */
+	protected function addMockConnection($model)
+	{
+		$model->setConnectionResolver(
+			$resolver = \Mockery::mock('Illuminate\Database\ConnectionResolverInterface')
+		);
+		$resolver->shouldReceive('connection')
+			->andReturn(\Mockery::mock('Illuminate\Database\Connection'));
+		$model->getConnection()
+			->shouldReceive('getQueryGrammar')
+				->andReturn(\Mockery::mock('Illuminate\Database\Query\Grammars\Grammar'));
+		$model->getConnection()
+			->shouldReceive('getPostProcessor')
+				->andReturn(\Mockery::mock('Illuminate\Database\Query\Processors\Processor'));
+	}
+
+	/**
+	 * Teardown test environment.
+	 */
+	public function tearDown()
+	{
+		\Mockery::close();
+	}
+
+	/**
+	 * Test Orchestra\Foundation\Model\UserMeta::users() method.
+	 *
+	 * @test
+	 */
+	public function testUsersMethod()
+	{
+		$model = new \Orchestra\Foundation\Model\UserMeta;
+
+		$this->addMockConnection($model);
+		
+		$stub = $model->users();
+
+		$this->assertInstanceOf('\Illuminate\Database\Eloquent\Relations\BelongsTo', $stub);
+		$this->assertInstanceOf('\Orchestra\Foundation\Model\User', $stub->getQuery()->getModel());
+	}
+}
