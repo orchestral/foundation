cc870cbd69bda85d96db3bdf167858d82714e8a9
crynobone <crynobone@gmail.com>
2013-04-15 15:58:23 +0800

    Add public assets
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ public/js/script.js
@@ -0,0 +1,53 @@
+jQuery(function startOrchestra ($) { 'use strict';
+	var ev = new Javie.Events();
+
+	$('select[role="switcher"]').each(function(i, item) {
+		var el = $(item);
+
+		el.toggleSwitch({
+			highlight: $(item).data("highlight"),
+			width: 25,
+			change : function () {
+				ev.fire('switcher.change', this);
+			}
+		})
+
+		el.css('display', 'none');
+	})
+
+	$('select[role!="switcher"]').select2();
+
+	$('*[role="tooltip"]').tooltip();
+
+	$('div.btn-group[data-toggle-name]').each(function() {
+		var group, form, name, hidden, buttons;
+
+		group   = $(this);
+		form    = group.parents('form').eq(0);
+		name    = group.attr('data-toggle-name');
+		hidden  = $('input[name="' + name + '"]', form);
+		buttons = $('button', group);
+
+		buttons.each(function(){
+			var button, setActive;
+
+			button = $(this);
+
+			setActive = function setActive() {
+				if(button.val() == hidden.val()) {
+					button.addClass('active');
+				}
+			};
+
+			button.on('click', function() {
+				buttons.removeClass('active');
+
+				hidden.val($(this).val());
+
+				setActive();
+			});
+
+			setActive();
+		});
+	});
+});
