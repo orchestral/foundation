3c434d16c99f73b90324fb20dada75b8e23feaeb
crynobone <crynobone@gmail.com>
2013-04-24 01:54:48 +0800

    Fixed usage of Orchestra\Messages.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/views/layout/widgets/messages.blade.php
+++ src/views/layout/widgets/messages.blade.php
@@ -1,13 +1,13 @@
 {{-- Retrieve the messages  --}}
 <?php $message = Orchestra\Messages::retrieve(); ?>
 
-@if ($message instanceof Orchestra\Messages)
+@if ($message instanceof Orchestra\Support\Messages)
 
 @foreach (array('error', 'info', 'success') as $key)
 @if ($message->has($key))
 	<?php 
 
-	$message->format('<div class="alert alert-'.$key.'">:message<button class="close" data-dismiss="alert">×</button></div>'); ?>
+	$message->setFormat('<div class="alert alert-'.$key.'">:message<button class="close" data-dismiss="alert">×</button></div>'); ?>
 	{{ implode('', $message->get($key)) }}
 @endif
 @endforeach
