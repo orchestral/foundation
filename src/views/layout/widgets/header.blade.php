acda1ccef9a6d20d68d256c889d2d16f80f4eed6
crynobone <crynobone@gmail.com>
2013-04-24 00:10:21 +0800

    add header widget.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/views/layout/widgets/header.blade.php
@@ -0,0 +1,21 @@
+{{-- Get the variable from Orchestra\Site --}}
+<?php
+
+$title       = Orchestra\Site::get('title');
+$description = Orchestra\Site::get('description'); ?>
+
+<div class="page-header">
+	@if (Orchestra\Site::get('header::add-button'))
+	<div class="pull-right">
+		<a href="{{ URL::current() }}/view" class="btn btn-primary">
+			{{ trans('orchestra/foundation::label.add') }}
+		</a>
+	</div>
+	@endif
+	
+	<h2>{{ $title ?: '' }}
+		@if ( ! empty($description))
+		<small>{{ $description ?: '' }}</small>
+		@endif
+	</h2>
+</div>
