fd72e81a9c3dec4462c0746923f0db4ef4a751ca
crynobone <crynobone@gmail.com>
2013-04-24 00:28:34 +0800

    Add title language replacement.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/lang/en/title.php
@@ -0,0 +1,41 @@
+<?php
+
+return array(
+	'home'     => 'Home',
+	'login'    => 'Login',
+	'logout'   => 'Logout',
+	'register' => 'Register',
+
+	'remember-me'     => 'Remember Me',
+	'forgot-password' => 'Forgot Password',
+
+	'account' => array(
+		'profile'  => 'Edit Profile',
+		'password' => 'Edit Password',
+	),
+
+	'extensions' => array(
+		'list'      => 'Extensions',
+		'configure' => 'Configure Extension',
+	),
+
+	'resources' => array(
+		'list'        => 'Resources',
+		'list-detail' => 'List of available resources',
+	),
+
+	'settings' => array(
+		'list'    => 'Settings',
+	),
+
+	'publisher' => array(
+		'ftp'         => 'FTP Credential',
+		'description' => 'We would need to have your credential in order to run the following task.'
+	),
+
+	'users' => array(
+		'list'   => 'Users',
+		'create' => 'Add User',
+		'update' => 'Edit User',
+	),
+);
