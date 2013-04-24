f68ac318477ae6b91a907d6d1e1f7393a7c6da08
crynobone <crynobone@gmail.com>
2013-04-24 10:13:29 +0800

    [#1] Add Orchestra\Model\Role events to update ACL.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/models/Role.php
+++ src/models/Role.php
@@ -1,6 +1,7 @@
 <?php namespace Orchestra\Model;
 
-use Illuminate\Database\Eloquent\Model as Eloquent;
+use Illuminate\Database\Eloquent\Model as Eloquent,
+	Orchestra\Acl;
 
 class Role extends Eloquent {
 
@@ -29,5 +30,31 @@ class Role extends Eloquent {
 	public function users() 
 	{
 		return $this->belongsToMany('\Orchestra\Model\User', 'user_role');
+	}
+
+	/**
+	 * The "booting" method of the model.
+	 *
+	 * @return void
+	 */
+	protected static function boot()
+	{
+		parent::boot();
+
+		static::creating(function ($role)
+		{
+			Acl::addRole($role->name);
+		});
+
+		static::updating(function ($role)
+		{
+			$oldName = $role->original['name'];
+			Acl::renameRole($oldName, $role->name);
+		});
+
+		static::deleting(function ($role)
+		{
+			Acl::removeRole($role->name);
+		});
 	}
 }
