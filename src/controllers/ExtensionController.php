cf8b730e622a7ea6838f2547d6fca0f4b34b525a
crynobone <crynobone@gmail.com>
2013-04-25 20:42:03 +0800

    Add basic extensions page
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/controllers/ExtensionController.php
+++ src/controllers/ExtensionController.php
@@ -1,3 +1,40 @@
 <?php namespace Orchestra\Foundation;
 
-class ExtensionController extends AdminController {}
+use View,
+	Orchestra\Extension,
+	Orchestra\Site;
+
+class ExtensionController extends AdminController {
+
+	/**
+	 * Construct Extensions Controller, only authenticated user should be
+	 * able to access this controller.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->beforeFilter('orchestra.auth');
+		$this->beforeFilter('orchestra.manage');
+	}
+
+	/**
+	 * List all available extensions.
+	 * 
+	 * GET (:orchestra)/extensions
+	 *
+	 * @access public
+	 * @return Response
+	 */
+	public function getIndex()
+	{
+		$extensions = Extension::detect();
+
+		Site::set('title', trans("orchestra/foundation::title.extensions.list"));
+
+		return View::make('orchestra/foundation::extensions.index', compact('extensions'));
+	}
+}
---
+++ src/views/extensions/index.blade.php
@@ -0,0 +1,66 @@
+@extends('orchestra/foundation::layout.main')
+
+<?php 
+
+use Illuminate\Support\Fluent,
+	Orchestra\Extension; ?>
+
+@section('content')
+
+<div class="row-fluid">
+
+	@include('orchestra/foundation::layout.widgets.header')
+
+	<table class="table table-bordered table-striped">
+		<thead>
+			<tr>
+				<th>{{ trans('orchestra/foundation::label.extensions.name') }}</th>
+				<th>{{ trans('orchestra/foundation::label.description') }}</th>
+			</tr>
+		</thead>
+		<tbody>
+			@if (empty($extensions))
+			<tr>
+				<td colspan="2">{{ trans('orchestra/foundation::label.no-extension') }}</td>
+			</tr>
+			@else
+			@foreach ($extensions as $name => $extension)
+			<?php $extension = new Fluent($extension); ?>
+			<tr>
+				<td>
+					<strong>
+						<?php 
+						$active  = Extension::isActive($name);
+						$started = Extension::started($name); ?>
+						@if ( ! ($started))
+							{{ $extension->name }}
+						@else
+							{{ Html::link(handles('orchestra/foundation::extensions/configure/'.$name), $extension->name) }}
+						@endif
+					</strong>
+					<div class="pull-right btn-group">
+						@if ( ! ($started or $active))
+							{{ Html::link(handles('orchestra/foundation::extensions/activate/'.$name), trans('orchestra/foundation::label.extensions.actions.activate'), array('class' => 'btn btn-primary btn-mini')) }}
+						@else
+							{{ Html::link(handles('orchestra/foundation::extensions/deactivate/'.$name), trans('orchestra/foundation::label.extensions.actions.deactivate'), array('class' => 'btn btn-warning btn-mini')) }}
+						@endif
+
+					</div>
+				</td>
+				<td>
+					<p>{{ $extension->description }}</p>
+
+					<span class="meta">
+						{{ trans('orchestra/foundation::label.extensions.version', array('version' => $extension->version )) }} |
+						{{ trans('orchestra/foundation::label.extensions.author', array('author' => Html::link($extension->url ?: '#', $extension->author))) }}
+					</span>
+				</td>
+			</tr>
+			@endforeach
+			@endif
+		</tbody>
+	</table>
+
+</div>
+
+@stop
