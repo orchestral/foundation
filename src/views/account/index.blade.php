b4a67ecd7e9b55d4c105bee1f46d9d3dadc527a6
crynobone <crynobone@gmail.com>
2013-06-08 09:52:44 +0800

    additional fixed to d231ff6
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/views/account/index.blade.php
+++ src/views/account/index.blade.php
@@ -4,12 +4,12 @@
 
 <div class="row">
 
-	<div class="span8">
+	<div class="col col-lg-8">
 		@include('orchestra/foundation::layout.widgets.header')
 		<?php echo $form; ?>
 	</div>
 
-	<div class="span4">
+	<div class="col col-lg-4">
 		@placeholder('orchestra.account')
 		@placeholder('orchestra.helps')
 	</div>
