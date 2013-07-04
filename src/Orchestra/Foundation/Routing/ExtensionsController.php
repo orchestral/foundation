5b8cd3c6fdf58f49453f6077dbef8ce6a193d979
crynobone <crynobone@gmail.com>
2013-07-05 02:19:28 +0800

    Fixed unable to use Publisher FTP since Illuminate\Filesystem\Filesystem::makeDirectory() throws PHP error when creating directory failed. Closes #60.

--- src/Orchestra/Foundation/Routing/ExtensionsController.php
+++ src/Orchestra/Foundation/Routing/ExtensionsController.php
@@ -3,6 +3,7 @@
 use Closure;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Fluent;
@@ -201,8 +202,19 @@ class ExtensionsController extends AdminController {
 	 */
 	protected function run($name, Closure $callback)
 	{
+		$publicPath = App::make('path.public');
+		$isWritable = File::isWritable($destination = "{$publicPath}/packages/{$name}");
+		
 		try
 		{
+			// Check if folder is writable via the web instance, this would 
+			// avoid issue running Orchestra Platform with debug as true where 
+			// creating/copying the directory would throw an ErrorException.
+			if ( ! $isWritable)
+			{
+				throw new FilePermissionException("[{$destination}] is not writable.");
+			}
+
 			call_user_func($callback, $name);
 		}
 		catch (FilePermissionException $e)
