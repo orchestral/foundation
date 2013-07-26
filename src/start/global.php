5e77d2e2d51f27aa03634b88cd2cb92bab0dea79
crynobone <crynobone@gmail.com>
2013-07-26 23:09:41 +0800

    Add safe mode notification when running from safe mode
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/start/global.php
+++ src/start/global.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Event;
 use Orchestra\Foundation\Services\UserMetaRepository;
 
 App::make('orchestra.memory')->extend('user', function ($app, $name)
@@ -16,4 +17,13 @@ App::bind('Orchestra\Foundation\Installation\InstallerInterface', function ()
 App::bind('Orchestra\Foundation\Installation\RequirementInterface', function ()
 {
 	return new Orchestra\Foundation\Installation\Requirement(App::make('app'));
+});
+
+
+Event::listen('orchestra.ready', function ()
+{
+	if ('on' === App::make('session')->get('orchestra.safemode'))
+	{
+		App::make('orchestra.messages')->add('info', trans('orchestra/foundation::response.safe-mode'));
+	}
 });
