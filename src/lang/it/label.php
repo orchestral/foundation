9e6cc4496a8b318884a73f89b2932455b78c966d
Matteo <matthweb@gmail.com>
2013-07-31 22:07:14 +0200

    Added Italian translation

---
+++ src/lang/it/label.php
@@ -0,0 +1,77 @@
+<?php
+
+return array(
+	'add'    => 'Aggiungi',
+	'delete' => 'Elimina',
+	'edit'   => 'Modifica',
+	'next'   => 'Successivo',
+	'submit' => 'Salva',
+	'view'   => 'Vedi',
+
+	'name'        => 'Nome',
+	'description' => 'Descrizione',
+
+	'no-data'      => 'Nessun dato al momento',
+	'no-extension' => 'Nessuna estensione disponibile al momento',
+
+	'account' => array(
+		'current_password' => 'Password attuale',
+		'new_password'     => 'Nuova password',
+		'confirm_password' => 'Conferma password',
+	),
+
+	'email' => array(
+		'driver'          => 'Driver',
+		'from'            => 'Indirizzo del mittente',
+		'host'            => 'Host',
+		'port'            => 'Porta',
+		'username'        => 'Nome utente',
+		'password'        => 'Password',
+		'encryption'      => 'Crittazione',
+		'change_password' => 'Cambia password',
+		'command'         => 'Comando sendmail',
+		'queue'           => 'Speidsci via Queue',
+	),
+
+	'extensions' => array(
+		'name'         => 'Estensione',
+		'author'       => 'Di :author',
+		'version'      => 'Versione :version',
+		'dependencies' => 'Dipendenze',
+		'update'       => 'Aggiorna',
+		'handles'      => 'Gestisce gli URL',
+
+		'actions' => array(
+			'activate'   => 'Attiva',
+			'deactivate' => 'Disattiva',
+			'update'     => 'Migra e pubblica',
+		),
+
+		'publisher' => array(
+			'host'            => 'Host',
+			'user'            => 'Nome utente',
+			'password'        => 'Password',
+			'connection-type' => 'Tipo di connessione',
+		),
+	),
+
+	'search'    => array(
+		'button'  => 'Cerca',
+		'filter'  => 'Filtro',
+		'keyword' => 'Parola chiave...',
+	),
+
+	'settings' => array(
+		'application'       => 'Applicazione',
+		'mail'              => 'E-mail',
+		'user-registration' => 'Consenti la registrazione degli utenti',
+	),
+
+	'users' => array(
+		'email'    => 'Indirizzo e-mail',
+		'username' => 'Nome utente',
+		'fullname' => 'Nome completo',
+		'password' => 'Password',
+		'roles'    => 'Ruoli',
+	),
+);
