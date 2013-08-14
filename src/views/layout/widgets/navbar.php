ebe0888048c4f65a790ab0939249ec82d477ea10
crynobone <crynobone@gmail.com>
2013-08-15 02:42:30 +0800

    Fixed CSS styling based on Bootstrap 3-RC2 changes, also closes #87.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/views/layout/widgets/navbar.php
+++ src/views/layout/widgets/navbar.php
@@ -2,21 +2,24 @@
 
 use Illuminate\Support\Facades\HTML;
 
-$attributes = HTML::decorate($navbar->attributes ?: array(), array('class' => 'navbar')); ?>
+$attributes = HTML::decorate($navbar->attributes ?: array(), array('class' => 'navbar', 'role' => 'navigation')); ?>
 
-<div<?php echo HTML::attributes($attributes); ?>>
+<nav<?php echo HTML::attributes($attributes); ?>>
 	<div class="container">
-		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".<?php echo $navbar->id; ?>-responsive-collapse">
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</button>
-		<a href="<?php echo $navbar->url; ?>" class="navbar-brand">
-			<?php echo $navbar->title; ?>
-		</a>
-		<div class="nav-collapse collapse <?php echo $navbar->id; ?>-responsive-collapse">	
-		<?php echo $navbar->menu; ?>
-		<?php echo $navbar->subMenu; ?>
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".<?php echo $navbar->id; ?>-responsive-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a href="<?php echo $navbar->url; ?>" class="navbar-brand">
+				<?php echo $navbar->title; ?>
+			</a>
+		</div>
+
+		<div class="collapse navbar-collapse <?php echo $navbar->id; ?>-responsive-collapse">	
+			<?php echo $navbar->menu; ?>
+			<?php echo $navbar->subMenu; ?>
 		</div>
 	</div>
-</div>
+</nav>
