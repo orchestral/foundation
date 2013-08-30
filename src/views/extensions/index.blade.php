9acf373bc91ee0d3754f83750e34e8c7c91759d6
crynobone <crynobone@gmail.com>
2013-08-30 15:52:12 +0800

    Separate large view into partial, to make it easier to be themed.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/views/extensions/index.blade.php
+++ src/views/extensions/index.blade.php
@@ -9,65 +9,7 @@ use Orchestra\Support\Facades\Extension; ?>
 
 <div class="row">
 	<div class="twelve columns white rounded box">
-		<table class="table table-striped">
-			<thead>
-				<tr>
-					<th><?php echo trans('orchestra/foundation::label.extensions.name'); ?></th>
-					<th><?php echo trans('orchestra/foundation::label.description'); ?></th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php if (empty($extensions)) : ?>
-				<tr>
-					<td colspan="2"><?php echo trans('orchestra/foundation::label.no-extension'); ?></td>
-				</tr>
-				<?php else : 
-				foreach ($extensions as $name => $extension) :
-					$extension = new Fluent($extension); ?>
-				<tr>
-					<td>
-						<strong>
-							<?php 
-							$active  = Extension::activated($name);
-							$started = Extension::started($name);
-							$uid     = str_replace('/', '.', $name);
-
-							if ( ! ($started)) :
-								echo $extension->name;
-							else : ?>
-								<a href="<?php echo handles("orchestra::extensions/configure/{$uid}"); ?>">
-									<?php echo $extension->name; ?>
-								</a>
-							<?php endif; ?>
-						</strong>
-						<div class="pull-right btn-group">
-							<?php if ( ! ($started or $active)) : ?>
-								<a href="<?php echo handles("orchestra::extensions/activate/{$uid}"); ?>" class="btn btn-primary btn-mini">
-									<?php echo trans('orchestra/foundation::label.extensions.actions.activate'); ?>
-								</a>
-							<?php else : ?>
-								<a href="<?php echo handles("orchestra::extensions/deactivate/{$uid}"); ?>" class="btn btn-warning btn-mini">
-									<?php echo trans('orchestra/foundation::label.extensions.actions.deactivate'); ?>
-								</a>
-							<?php endif; ?>
-
-						</div>
-					</td>
-					<td>
-						<p>
-							<?php echo $extension->description; ?>
-						</p>
-
-						<span class="meta">
-							<?php echo trans('orchestra/foundation::label.extensions.version', array('version' => $extension->version )); ?> |
-							<?php echo trans('orchestra/foundation::label.extensions.author', array('author' => sprintf('<a href="%s">%s</a>', $extension->url ?: '#', $extension->author))); ?>
-						</span>
-					</td>
-				</tr>
-				<?php endforeach;
-				endif; ?>
-			</tbody>
-		</table>
+		@include('orchestra/foundation::extensions.table')
 	</div>
 </div>
 
--- src/views/resources/page.blade.php
+++ src/views/resources/page.blade.php
@@ -5,15 +5,7 @@
 <div class="row">
 
 	<div class="three columns">
-		<div class="list-group">
-			<?php foreach ($resources['list'] as $name => $resource) : ?>
-			<a href="<?php echo resources($name); ?>" 
-				class="list-group-item <?php echo Request::is("*/resources/{$name}*") ? 'active' : ''; ?>">
-				<?php echo $resource->name; ?>
-				<span class="glyphicon glyphicon-chevron-right pull-right"></span>
-			</a>
-			<?php endforeach; ?>
-		</div>
+		@include('orchestra/foundation::resources.list')
 		
 		@placeholder("orchestra.resources: {$resource->name}")
 		@placeholder('orchestra.resources')
