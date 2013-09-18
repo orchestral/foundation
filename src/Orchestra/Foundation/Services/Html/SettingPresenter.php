f66b09056f3334c1514d54f1dfd00c60b54b9d68
crynobone <crynobone@gmail.com>
2013-09-18 23:33:40 +0800

    Reduce complexity on Orchestra\Foundation\Services\Html\SettingPresenter. Closes #99.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Orchestra/Foundation/Services/Html/SettingPresenter.php
+++ src/Orchestra/Foundation/Services/Html/SettingPresenter.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\HTML;
 use Illuminate\Support\Facades\Form as F;
 use Orchestra\Support\Facades\Form;
 use Orchestra\Support\Facades\Table;
+use Orchestra\Html\Form\FormBuilder;
 
 class SettingPresenter {
 
@@ -15,7 +16,9 @@ class SettingPresenter {
 	 */
 	public function form($model)
 	{
-		return Form::of('orchestra.settings', function ($form) use ($model)
+		$self = $this;
+		
+		return Form::of('orchestra.settings', function ($form) use ($self, $model)
 		{
 			$form->with($model);
 			$form->layout('orchestra/foundation::components.form');
@@ -24,99 +27,121 @@ class SettingPresenter {
 				'method' => 'POST',
 			));
 
-			$form->fieldset(trans('orchestra/foundation::label.settings.application'), function ($fieldset)
+			$self->applicationForm($form);
+			$self->mailerForm($form, $model);
+		});
+	}
+
+	/**
+	 * Form view generator for application configuration.
+	 * 
+	 * @return \Orchestra\Html\Form\FormBuilder $form
+	 * @return void
+	 */
+	public function applicationForm($form)
+	{
+		$form->fieldset(trans('orchestra/foundation::label.settings.application'), function ($fieldset)
+		{
+			$fieldset->control('input:text', 'site_name', function ($control)
 			{
-				$fieldset->control('input:text', 'site_name', function ($control)
-				{
-					$control->label(trans('orchestra/foundation::label.name'));
-				});
-
-				$fieldset->control('textarea', 'site_description', function ($control)
-				{
-					$control->label(trans('orchestra/foundation::label.description'));
-					$control->attributes(array('rows' => 3));
-				});
-
-				$fieldset->control('select', 'site_registrable', function ($control)
-				{
-					$control->label(trans('orchestra/foundation::label.settings.user-registration'));
-					$control->attributes(array('role' => 'switcher'));
-					$control->options(array(
-						'yes' => 'Yes',
-						'no'  => 'No',
-					));
-				});
+				$control->label(trans('orchestra/foundation::label.name'));
 			});
 
-			$form->fieldset(trans('orchestra/foundation::label.settings.mail'), function ($fieldset) use ($model)
+			$fieldset->control('textarea', 'site_description', function ($control)
 			{
-				$fieldset->control('select', 'email_driver', function ($control)
-				{
-					$control->label(trans('orchestra/foundation::label.email.driver'));
-					$control->options(array(
-						'mail'     => 'Mail',
-						'smtp'     => 'SMTP',
-						'sendmail' => 'Sendmail'
-					));
-				});
+				$control->label(trans('orchestra/foundation::label.description'));
+				$control->attributes(array('rows' => 3));
+			});
 
-				$fieldset->control('input:text', 'email_host', function ($control)
-				{
-					$control->label(trans('orchestra/foundation::label.email.host'));
-				});
+			$fieldset->control('select', 'site_registrable', function ($control)
+			{
+				$control->label(trans('orchestra/foundation::label.settings.user-registration'));
+				$control->attributes(array('role' => 'switcher'));
+				$control->options(array(
+					'yes' => 'Yes',
+					'no'  => 'No',
+				));
+			});
+		});
+	}
+	
+	/**
+	 * Form view generator for email configuration.
+	 * 
+	 * @param  \Orchestra\Html\Form\FormBuilder $form
+	 * @param  \Illuminate\Support\Fluent       $model
+	 * @return void
+	 */
+	public function mailerForm($form, $model)
+	{
+		$form->fieldset(trans('orchestra/foundation::label.settings.mail'), function ($fieldset) use ($model)
+		{
+			$fieldset->control('select', 'email_driver', function ($control)
+			{
+				$control->label(trans('orchestra/foundation::label.email.driver'));
+				$control->options(array(
+					'mail'     => 'Mail',
+					'smtp'     => 'SMTP',
+					'sendmail' => 'Sendmail'
+				));
+			});
 
-				$fieldset->control('input:text', 'email_port', function ($control)
-				{
-					$control->label(trans('orchestra/foundation::label.email.port'));
-				});
-				
-				$fieldset->control('input:text', 'email_address', function ($control)
-				{
-					$control->label(trans('orchestra/foundation::label.email.from'));
-				});
+			$fieldset->control('input:text', 'email_host', function ($control)
+			{
+				$control->label(trans('orchestra/foundation::label.email.host'));
+			});
 
-				$fieldset->control('input:text', 'email_username', function ($control)
-				{
-					$control->label(trans('orchestra/foundation::label.email.username'));
-				});
+			$fieldset->control('input:text', 'email_port', function ($control)
+			{
+				$control->label(trans('orchestra/foundation::label.email.port'));
+			});
+			
+			$fieldset->control('input:text', 'email_address', function ($control)
+			{
+				$control->label(trans('orchestra/foundation::label.email.from'));
+			});
 
-				$fieldset->control('input:password', 'email_password', function ($control) use ($model)
-				{
-					$help = array(
-						HTML::create('span', str_repeat('*', strlen($model->email_smtp_password))),
-						'&nbsp;&nbsp;',
-						HTML::link('#', trans('orchestra/foundation::label.email.change_password'), array(
-							'id' => 'change_password_button',
-							'class' => 'btn btn-mini btn-warning',
-						)),
-						F::hidden('change_password', 'no'),
-					);
+			$fieldset->control('input:text', 'email_username', function ($control)
+			{
+				$control->label(trans('orchestra/foundation::label.email.username'));
+			});
 
-					$control->label(trans('orchestra/foundation::label.email.password'));
-					$control->help(HTML::create('span', HTML::raw(implode('', $help)), array(
-						'id' => 'change_password_container',
-					)));
-				});
-				
-				$fieldset->control('input:text', 'email_encryption', function ($control)
-				{
-					$control->label(trans('orchestra/foundation::label.email.encryption'));
-				});
+			$fieldset->control('input:password', 'email_password', function ($control) use ($model)
+			{
+				$help = array(
+					HTML::create('span', str_repeat('*', strlen($model->email_smtp_password))),
+					'&nbsp;&nbsp;',
+					HTML::link('#', trans('orchestra/foundation::label.email.change_password'), array(
+						'id' => 'change_password_button',
+						'class' => 'btn btn-mini btn-warning',
+					)),
+					F::hidden('change_password', 'no'),
+				);
 
-				$fieldset->control('input:text', 'email_sendmail', function ($control)
-				{
-					$control->label(trans('orchestra/foundation::label.email.command'));
-				});
+				$control->label(trans('orchestra/foundation::label.email.password'));
+				$control->help(HTML::create('span', HTML::raw(implode('', $help)), array(
+					'id' => 'change_password_container',
+				)));
+			});
+			
+			$fieldset->control('input:text', 'email_encryption', function ($control)
+			{
+				$control->label(trans('orchestra/foundation::label.email.encryption'));
+			});
 
-				$fieldset->control('select', 'email_queue', function ($control)
-				{
-					$control->label(trans('orchestra/foundation::label.email.queue'));
-					$control->attributes(array('role' => 'switcher'));
-					$control->options(array(
-						'yes' => 'Yes',
-						'no'  => 'No',
-					));
-				});
+			$fieldset->control('input:text', 'email_sendmail', function ($control)
+			{
+				$control->label(trans('orchestra/foundation::label.email.command'));
+			});
+
+			$fieldset->control('select', 'email_queue', function ($control)
+			{
+				$control->label(trans('orchestra/foundation::label.email.queue'));
+				$control->attributes(array('role' => 'switcher'));
+				$control->options(array(
+					'yes' => 'Yes',
+					'no'  => 'No',
+				));
 			});
 		});
 	}
