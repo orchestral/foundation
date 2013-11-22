6e0d32bffd6801588910ce7a9a6abbbc17f27a86
crynobone <crynobone@gmail.com>
2013-11-22 18:24:44 +0800

    Separate welcome note as a partial
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/views/dashboard/index.blade.php
+++ src/views/dashboard/index.blade.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\View; ?>
 
 <div class="row">
 	<?php if (count($panes) > 0) :
-	
+
 	$panes->add('mini-profile', '<')->title('Mini Profile')
 		->attributes(array('class' => 'three columns widget'))
 		->content(View::make('orchestra/foundation::layout.widgets.miniprofile'));
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\View; ?>
 	foreach ($panes as $id => $pane) : ?>
 		<div<?php echo HTML::attributes(HTML::decorate($pane->attributes, array('class' => 'panel'))); ?>>
 		<?php if ( ! empty($pane->html)) :
-			echo $pane->html; 
+			echo $pane->html;
 		else : ?>
 			<div class="panel-heading"><?php echo $pane->title; ?></div>
 			<?php echo $pane->content; ?>
@@ -25,13 +25,7 @@ use Illuminate\Support\Facades\View; ?>
 		</div>
 	<?php endforeach;
 	else : ?>
-	<div class="jumbotron">
-		<h2>Welcome to your new Orchestra Platform site!</h2>
-		<p>
-			If you need help getting started, check out our documentation on First Steps with Orchestra Platform. 
-			If you’d rather dive right in, here are a few things most people do first when they set up a new Orchestra Platform site. 
-		</p>
-	</div>
+	@include('orchestra/foundation::dashboard._welcome')
 	<?php endif; ?>
 </div>
 
