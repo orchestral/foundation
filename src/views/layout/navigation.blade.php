df4ac2e0d909567d4b9e4d9e6dee02127b601c5b
crynobone <crynobone@gmail.com>
2013-12-18 15:54:02 +0800

    Fixed regression bug where there missing container on main navigation.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/views/layout/navigation.blade.php
+++ src/views/layout/navigation.blade.php
@@ -1,35 +1,38 @@
-<?php 
+<?php
 
-use Illuminate\Support\Fluent;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\View;
-use Orchestra\Support\Facades\App;
+use Orchestra\Support\Facades\App; ?>
 
-$navbar = new Fluent(array(
-	'id'         => 'main',
-	'title'      => memorize('site.name', 'Orchestra'),
-	'url'        => handles('orchestra::/'),
-	'attributes' => array('class' => 'navbar-fixed-top navbar-inverse'),
-	'left'       => View::make('orchestra/foundation::layout.widgets.menu', array('menu' => App::menu('orchestra'))),
-	'right'      => View::make('orchestra/foundation::layout.widgets.usernav'),
-)); ?>
-
-@decorator('navbar', $navbar)
+<header class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".main-responsive-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a href="<?php echo handles('orchestra::/'); ?>" class="navbar-brand">
+				<?php echo memorize('site.name', 'Orchestra Platform'); ?>
+			</a>
+		</div>
+		<div class="collapse navbar-collapse main-responsive-collapse">
+			@include('orchestra/foundation::components.menu', array('menu' => App::menu('orchestra')))
+			@include('orchestra/foundation::components.usernav')
+		</div>
+	</div>
+</header>
 
 <?php if ( ! Auth::check()) : ?>
-
 <script>
 jQuery(function ($) {
 	$('a[rel="user-menu"]').on('click', function (e) {
 		e.preventDefault();
-		
+
 		window.location.href = "<?php echo handles('orchestra::login'); ?>";
 
 		return false;
 	});
 });
 </script>
-
 <?php endif; ?>
-
 <br>
