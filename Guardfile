02c88b653b5afb5e300e9b08810d4b0dd64d4903
crynobone <crynobone@gmail.com>
2014-02-03 23:58:46 +0800

    Add gulp alternative instead of utilizing ant and guard.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- Guardfile
+++ Guardfile
@@ -6,17 +6,3 @@ guard :phpunit2, :all_on_start => false, :tests_path => 'tests/', :cli => '--col
 	# Save app/models/User.php, and it will run app/tests/models/UserTest.php
 	watch(%r{^src/(.+)/(.+)\.php$}) { |m| "tests/#{m[1]}/#{m[2]}Test.php"}
 end
-
-guard 'shell' do
-	watch(%r{^public/css/(.+\.less)}) { |m|
-		n m[0], "Changed"
-		`ant minify-css`
-	}
-end
-
-guard 'shell' do
-	watch(%r{^public/js/(.+\.coffee)}) { |m|
-		n m[0], "Changed"
-		`ant minify-js`
-	}
-end
---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    csso = require('gulp-csso'),
+    less = require('gulp-less'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');
+
+// Less
+gulp.task('css', function () {
+    return gulp.src('public/css/orchestra.less')
+        .pipe(less())
+        .pipe(csso())
+        .pipe(gulp.dest('public/css'));
+});
+
+// Coffee
+gulp.task('js', function () {
+    return gulp.src('public/js/orchestra.coffee')
+        .pipe(coffee().on('error', gutil.log))
+        .pipe(gulp.dest('public/js'));
+});
+
+// Minify JavaScript
+gulp.task('uglify', function () {
+    var options = {
+        outSourceMaps: false,
+        output: {
+            max_line_len: 150
+        }
+    };
+
+    return gulp.src('public/js/orchestra.js')
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify(options))
+        .pipe(gulp.dest('public/js'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('public/css/orchestra.less', ['css']);
+    gulp.watch('public/css/orchestra.coffee', ['js']);
+    gulp.watch('public/css/orchestra.js', ['uglify']);
+});
+
+// Default task.
+gulp.task('default', ['css', 'js', 'uglify', 'watch']);
