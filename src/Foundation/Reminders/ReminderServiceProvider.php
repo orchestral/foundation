76aef2750842d23a8a714e3f007f7b9582933ff0
crynobone <crynobone@gmail.com>
2014-07-24 20:31:13 +0800

    auth.reminder service location should be registered using bind shared.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Foundation/Reminders/ReminderServiceProvider.php
+++ src/Foundation/Reminders/ReminderServiceProvider.php
@@ -16,7 +16,7 @@ class ReminderServiceProvider extends \Illuminate\Auth\Reminders\ReminderService
      */
     protected function registerPasswordBroker()
     {
-        $this->app['auth.reminder'] = $this->app->share(function ($app) {
+        $this->app->bindShared('auth.reminder', function ($app) {
             // The reminder repository is responsible for storing the user e-mail addresses
             // and password reset tokens. It will be used to verify the tokens are valid
             // for the given e-mail addresses. We will resolve an implementation here.
@@ -31,12 +31,7 @@ class ReminderServiceProvider extends \Illuminate\Auth\Reminders\ReminderService
             // The password broker uses the reminder repository to validate tokens and send
             // reminder e-mails, as well as validating that password reset process as an
             // aggregate service of sorts providing a convenient interface for resets.
-            return new PasswordBroker(
-                $reminders,
-                $users,
-                $notifier,
-                $view
-            );
+            return new PasswordBroker($reminders, $users, $notifier, $view);
         });
     }
 
