25cdee9fb28145330ddfae318b4fa75da6c2a059
crynobone <crynobone@gmail.com>
2014-09-02 01:36:47 +0800

    Utilize Illuminate\Support\AggregateServiceProvider.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Foundation/ConsoleSupportServiceProvider.php
+++ src/Foundation/ConsoleSupportServiceProvider.php
@@ -1,8 +1,8 @@
 <?php namespace Orchestra\Foundation;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\AggregateServiceProvider;
 
-class ConsoleSupportServiceProvider extends ServiceProvider
+class ConsoleSupportServiceProvider extends AggregateServiceProvider
 {
     /**
      * The provider class names.
@@ -19,48 +19,9 @@ class ConsoleSupportServiceProvider extends ServiceProvider
     );
 
     /**
-     * An array of the service provider instances.
-     *
-     * @var array
-     */
-    protected $instances = array();
-
-    /**
      * Indicates if loading of the provider is deferred.
      *
      * @var boolean
      */
     protected $defer = true;
-
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->instances = array();
-
-        foreach ($this->providers as $provider) {
-            $this->instances[] = $this->app->register($provider);
-        }
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        $provides = array();
-
-        foreach ($this->providers as $provider) {
-            $instance = $this->app->resolveProviderClass($provider);
-
-            $provides = array_merge($provides, $instance->provides());
-        }
-
-        return $provides;
-    }
 }
