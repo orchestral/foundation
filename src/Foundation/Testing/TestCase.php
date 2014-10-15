69e811ec738eaff4646be4a20bdefeb7a58b177d
crynobone <crynobone@gmail.com>
2014-10-15 21:59:44 +0800

    Tweak testcase setup.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Foundation/Testing/TestCase.php
+++ src/Foundation/Testing/TestCase.php
@@ -12,43 +12,41 @@ abstract class TestCase extends TestbenchTestCase
     protected function getApplicationAliases()
     {
         return [
-            'App'             => 'Illuminate\Support\Facades\App',
-            'Artisan'         => 'Illuminate\Support\Facades\Artisan',
-            'Asset'           => 'Orchestra\Support\Facades\Asset',
-            'Auth'            => 'Illuminate\Support\Facades\Auth',
-            'Blade'           => 'Illuminate\Support\Facades\Blade',
-            'Cache'           => 'Illuminate\Support\Facades\Cache',
-            'Config'          => 'Illuminate\Support\Facades\Config',
-            'Cookie'          => 'Illuminate\Support\Facades\Cookie',
-            'Crypt'           => 'Illuminate\Support\Facades\Crypt',
-            'DB'              => 'Illuminate\Support\Facades\DB',
-            'Eloquent'        => 'Illuminate\Database\Eloquent\Model',
-            'Event'           => 'Illuminate\Support\Facades\Event',
-            'Facile'          => 'Orchestra\Support\Facades\Facile',
-            'File'            => 'Illuminate\Support\Facades\File',
-            'Form'            => 'Orchestra\Support\Facades\Form',
-            'Hash'            => 'Illuminate\Support\Facades\Hash',
-            'HTML'            => 'Orchestra\Support\Facades\HTML',
-            'Input'           => 'Illuminate\Support\Facades\Input',
-            'Lang'            => 'Illuminate\Support\Facades\Lang',
-            'Log'             => 'Illuminate\Support\Facades\Log',
-            'Mail'            => 'Illuminate\Support\Facades\Mail',
-            'Paginator'       => 'Illuminate\Support\Facades\Paginator',
-            'Password'        => 'Illuminate\Support\Facades\Password',
-            'Queue'           => 'Illuminate\Support\Facades\Queue',
-            'Redirect'        => 'Illuminate\Support\Facades\Redirect',
-            'Redis'           => 'Illuminate\Support\Facades\Redis',
-            'Request'         => 'Illuminate\Support\Facades\Request',
-            'Response'        => 'Illuminate\Support\Facades\Response',
-            'Route'           => 'Illuminate\Support\Facades\Route',
-            'Schema'          => 'Illuminate\Support\Facades\Schema',
-            'Seeder'          => 'Illuminate\Database\Seeder',
-            'Session'         => 'Illuminate\Support\Facades\Session',
-            'Str'             => 'Orchestra\Support\Str',
-            'Theme'           => 'Orchestra\Support\Facades\Theme',
-            'URL'             => 'Illuminate\Support\Facades\URL',
-            'Validator'       => 'Illuminate\Support\Facades\Validator',
-            'View'            => 'Illuminate\Support\Facades\View',
+            'App'        => 'Illuminate\Support\Facades\App',
+            'ACL'        => 'Orchestra\Support\Facades\ACL',
+            'Artisan'    => 'Illuminate\Support\Facades\Artisan',
+            'Asset'      => 'Orchestra\Support\Facades\Asset',
+            'Auth'       => 'Illuminate\Support\Facades\Auth',
+            'Blade'      => 'Illuminate\Support\Facades\Blade',
+            'Cache'      => 'Illuminate\Support\Facades\Cache',
+            'Config'     => 'Illuminate\Support\Facades\Config',
+            'Cookie'     => 'Illuminate\Support\Facades\Cookie',
+            'Crypt'      => 'Illuminate\Support\Facades\Crypt',
+            'DB'         => 'Illuminate\Support\Facades\DB',
+            'Event'      => 'Illuminate\Support\Facades\Event',
+            'Facile'     => 'Orchestra\Support\Facades\Facile',
+            'File'       => 'Illuminate\Support\Facades\File',
+            'Form'       => 'Orchestra\Support\Facades\Form',
+            'Hash'       => 'Illuminate\Support\Facades\Hash',
+            'HTML'       => 'Orchestra\Support\Facades\HTML',
+            'Input'      => 'Illuminate\Support\Facades\Input',
+            'Lang'       => 'Illuminate\Support\Facades\Lang',
+            'Log'        => 'Illuminate\Support\Facades\Log',
+            'Mail'       => 'Illuminate\Support\Facades\Mail',
+            'Paginator'  => 'Illuminate\Support\Facades\Paginator',
+            'Password'   => 'Illuminate\Support\Facades\Password',
+            'Queue'      => 'Illuminate\Support\Facades\Queue',
+            'Redirect'   => 'Illuminate\Support\Facades\Redirect',
+            'Redis'      => 'Illuminate\Support\Facades\Redis',
+            'Request'    => 'Illuminate\Support\Facades\Request',
+            'Response'   => 'Illuminate\Support\Facades\Response',
+            'Route'      => 'Illuminate\Support\Facades\Route',
+            'Schema'     => 'Illuminate\Support\Facades\Schema',
+            'Session'    => 'Illuminate\Support\Facades\Session',
+            'Theme'      => 'Orchestra\Support\Facades\Theme',
+            'URL'        => 'Illuminate\Support\Facades\URL',
+            'Validator'  => 'Illuminate\Support\Facades\Validator',
+            'View'       => 'Illuminate\Support\Facades\View',
         ];
     }
 
@@ -85,7 +83,10 @@ abstract class TestCase extends TestbenchTestCase
      */
     protected function getApplicationProviders()
     {
-        return array(
+        return [
+            /*
+             * Laravel Framework Service Providers...
+             */
             'Illuminate\Foundation\Providers\ArtisanServiceProvider',
             'Illuminate\Cache\CacheServiceProvider',
             'Illuminate\Foundation\Providers\ConsoleSupportServiceProvider',
@@ -93,21 +94,21 @@ abstract class TestCase extends TestbenchTestCase
             'Illuminate\Database\DatabaseServiceProvider',
             'Illuminate\Encryption\EncryptionServiceProvider',
             'Illuminate\Filesystem\FilesystemServiceProvider',
-            'Illuminate\Foundation\Providers\FormRequestServiceProvider',
+            'Illuminate\Foundation\Providers\FoundationServiceProvider',
             'Illuminate\Hashing\HashServiceProvider',
             'Illuminate\Log\LogServiceProvider',
             'Illuminate\Mail\MailServiceProvider',
-            'Illuminate\Database\MigrationServiceProvider',
             'Illuminate\Pagination\PaginationServiceProvider',
             'Illuminate\Foundation\Providers\PublisherServiceProvider',
             'Illuminate\Queue\QueueServiceProvider',
             'Illuminate\Redis\RedisServiceProvider',
-            'Illuminate\Database\SeedServiceProvider',
             'Illuminate\Session\SessionServiceProvider',
-            'Illuminate\Translation\TranslationServiceProvider',
             'Illuminate\Validation\ValidationServiceProvider',
             'Illuminate\View\ViewServiceProvider',
 
+            /*
+             * Orchestra Platform Service Providers...
+             */
             'Orchestra\Asset\AssetServiceProvider',
             'Orchestra\Auth\AuthServiceProvider',
             'Orchestra\Routing\ControllerServiceProvider',
@@ -124,13 +125,14 @@ abstract class TestCase extends TestbenchTestCase
             'Orchestra\Publisher\PublisherServiceProvider',
             'Orchestra\Resources\ResourcesServiceProvider',
             'Orchestra\Foundation\SiteServiceProvider',
+            'Orchestra\Translation\TranslationServiceProvider',
             'Orchestra\View\ViewServiceProvider',
             'Orchestra\Widget\WidgetServiceProvider',
 
-            //'Orchestra\Foundation\FilterServiceProvider',
             'Orchestra\Foundation\ConsoleSupportServiceProvider',
+            'Orchestra\Foundation\FilterServiceProvider',
             'Orchestra\Foundation\FoundationServiceProvider',
-        );
+        ];
     }
 
     /**
@@ -140,6 +142,16 @@ abstract class TestCase extends TestbenchTestCase
      */
     protected function getPackageProviders()
     {
-        return array();
+        return [];
+    }
+
+     /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application   $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
     }
 }
