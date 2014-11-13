b2f95a5e29f256fae0dca18cca7dc82dfa30f8b8
crynobone <crynobone@gmail.com>
2014-11-13 10:56:38 +0800

    Convert verify csrf token to class.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Foundation/Filters/VerifyCsrfToken.php
@@ -0,0 +1,24 @@
+<?php namespace Orchestra\Foundation\Filters;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
+use Illuminate\Session\TokenMismatchException;
+use Symfony\Component\Security\Core\Util\StringUtils;
+
+class VerifyCsrfToken
+{
+    /**
+     * Run the request filter.
+     *
+     * @param  \Illuminate\Routing\Route  $route
+     * @param  \Illuminate\Http\Request   $request
+     * @return mixed
+     * @throws \Illuminate\Session\TokenMismatchException
+    */
+    public function filter(Route $route, Request $request)
+    {
+        if (! StringUtils::equals($request->getSession()->token(), $request->input('_token'))) {
+            throw new TokenMismatchException;
+        }
+    }
+}
