be1bad0b74e043fa1cb52fe576d642b904fc9b20
crynobone <crynobone@gmail.com>
2014-11-26 21:05:11 +0800

    Move FilterServiceProvider.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Foundation/FilterServiceProvider.php
+++ src/Foundation/FilterServiceProvider.php
@@ -1,21 +1,8 @@
 <?php namespace Orchestra\Foundation;
 
-use Orchestra\Support\Providers\FilterServiceProvider as ServiceProvider;
+use Orchestra\Foundation\Providers\FilterServiceProvider as ServiceProvider;
 
 class FilterServiceProvider extends ServiceProvider
 {
-    /**
-     * All available route filters.
-     *
-     * @var array
-     */
-    protected $filters = [
-        'orchestra.auth' => 'Orchestra\Foundation\Filters\Authenticate',
-        'orchestra.csrf' => 'Orchestra\Foundation\Filters\VerifyCsrfToken',
-        'orchestra.guest' => 'Orchestra\Foundation\Filters\IsGuest',
-        'orchestra.installable' => 'Orchestra\Foundation\Filters\CanBeInstalled',
-        'orchestra.installed' => 'Orchestra\Foundation\Filters\IsInstalled',
-        'orchestra.manage' => 'Orchestra\Foundation\Filters\CanManage',
-        'orchestra.registrable' => 'Orchestra\Foundation\Filters\IsRegistrable',
-    ];
+    //
 }
---
+++ src/Foundation/Providers/FilterServiceProvider.php
@@ -0,0 +1,21 @@
+<?php namespace Orchestra\Foundation\Providers;
+
+use Orchestra\Support\Providers\FilterServiceProvider as ServiceProvider;
+
+class FilterServiceProvider extends ServiceProvider
+{
+    /**
+     * All available route filters.
+     *
+     * @var array
+     */
+    protected $filters = [
+        'orchestra.auth' => 'Orchestra\Foundation\Filters\Authenticate',
+        'orchestra.csrf' => 'Orchestra\Foundation\Filters\VerifyCsrfToken',
+        'orchestra.guest' => 'Orchestra\Foundation\Filters\IsGuest',
+        'orchestra.installable' => 'Orchestra\Foundation\Filters\CanBeInstalled',
+        'orchestra.installed' => 'Orchestra\Foundation\Filters\IsInstalled',
+        'orchestra.manage' => 'Orchestra\Foundation\Filters\CanManage',
+        'orchestra.registrable' => 'Orchestra\Foundation\Filters\IsRegistrable',
+    ];
+}
