eec193fac5b6d2e7109f10d4eddc6548ee97f58f
crynobone <crynobone@gmail.com>
2015-01-06 16:09:52 +0800

    Add ArtisanServiceProvider.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Providers/ArtisanServiceProvider.php
@@ -0,0 +1,19 @@
+<?php namespace Orchestra\Foundation\Providers;
+
+use Orchestra\Config\Console\ConfigCacheCommand;
+use Illuminate\Foundation\Providers\ArtisanServiceProvider as ServiceProvider;
+
+class ArtisanServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerConfigCacheCommand()
+    {
+        $this->app->singleton('command.config.cache', function ($app) {
+            return new ConfigCacheCommand($app['files']);
+        });
+    }
+}
