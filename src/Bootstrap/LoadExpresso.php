270d49b85a06ee6817cae5c296977b06e2f563eb
crynobone <crynobone@gmail.com>
2015-01-16 07:11:21 +0800

    Fixes orchestral/platform#44
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Bootstrap/LoadExpresso.php
+++ src/Bootstrap/LoadExpresso.php
@@ -33,8 +33,9 @@ class LoadExpresso
             $decorator = '$1<?php echo app("orchestra.decorator")->render($2); ?>';
             $placeholder = '$1<?php $__ps = app("orchestra.widget")->make("placeholder.".$2); '
                                 .'foreach ($__ps as $__p) { echo value($__p->value ?:""); } ?>';
+            $title = '$1<?php echo app("html")->title($2); ?>';
 
-            foreach (compact('decorator', 'placeholder') as $name => $replacement) {
+            foreach (compact('decorator', 'placeholder', 'title') as $name => $replacement) {
                 $view = preg_replace('/(\s*)@'.$name.'\s?\(\s*(.*)\)/', $replacement, $view);
             }
 
@@ -67,9 +68,9 @@ class LoadExpresso
     {
         $html = $app['html'];
 
-        $html->macro('title', function () use ($html) {
+        $html->macro('title', function ($pageTitle = null) use ($html) {
             $siteTitle = $title = memorize('site.name');
-            $pageTitle = trim(get_meta('title', ''));
+            $pageTitle = $pageTitle ?: trim(get_meta('title', ''));
             $format    = memorize('site.format.title', ':pageTitle &mdash; :siteTitle');
 
             if (! empty($pageTitle)) {
