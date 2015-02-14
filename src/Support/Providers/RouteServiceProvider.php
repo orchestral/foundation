3c29497156aa71d1dfb677ec2f7420217ce97f08
crynobone <crynobone@gmail.com>
2015-02-14 19:41:29 +0800

    Add route service provider, should be useful when defining extension/modules routes.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Support/Providers/RouteServiceProvider.php
@@ -0,0 +1,57 @@
+<?php namespace Orchestra\Support\Providers;
+
+use Illuminate\Routing\Router;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+abstract class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    public function boot(Router $router)
+    {
+        if (! $this->app->routesAreCached()) {
+            $this->loadRoutes();
+        }
+    }
+
+    /**
+     * Load the backend routes file for the application.
+     *
+     * @param  string  $path
+     * @param  string|null  $namespace
+     * @return void
+     */
+    protected function loadBackendRoutesFrom($path, $namespace = null)
+    {
+        $foundation = $this->app['orchestra.app'];
+
+        $foundation->namespaced($namespace, function(Router $router) use ($path) {
+            require $path;
+        });
+    }
+
+    /**
+     * Load the frontend routes file for the application.
+     *
+     * @param  string  $path
+     * @param  string|null  $namespace
+     * @return void
+     */
+    protected function loadFrontendRoutesFrom($path, $namespace = null)
+    {
+        $router = $this->app['router'];
+
+        if (is_null($namespace)) {
+            return require $path;
+        }
+
+        $router->group(['namespace' => $namespace], function(Router $router) use ($path) {
+            require $path;
+        });
+    }
+
+}
