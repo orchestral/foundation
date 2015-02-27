7824e040cf59c410854f06bb123d25b6b59408b8
crynobone <crynobone@gmail.com>
2015-02-28 07:52:34 +0800

    Back to basic.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build/travis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TRAVIS_PHP_VERSION=$1;
-
-if [[ "$TRAVIS_PHP_VERSION" == *5.6* ]]; then
-    composer require "satooshi/php-coveralls:~0.6.1" "symfony/config=~2.0" "symfony/yaml=~2.0" --prefer-source --no-interaction --dev;
-else
-    composer install --prefer-source --no-interaction
-fi
