308ddf1f5b15d394f0233b865627a5758332abb7
crynobone <crynobone@gmail.com>
2015-02-28 07:44:57 +0800

    Tweak build script.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build/travis.sh
+++ build/travis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-travis_retry composer self-update
+
 
 if [[ "$TRAVIS_PHP_VERSION" == *5.6* ]]; then
-    travis_retry composer require "satooshi/php-coveralls:~0.6.1" "symfony/config=~2.0" --prefer-source --no-interaction --dev;
+    composer require "satooshi/php-coveralls:~0.6.1" "symfony/config=~2.0" --prefer-source --no-interaction --dev;
 else
-    travis_retry composer install --prefer-source --no-interaction
+    composer install --prefer-source --no-interaction
 fi
