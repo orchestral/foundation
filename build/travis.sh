6456445d2f90fbf5c92bb9fa9e5ea1915a001d0b
crynobone <crynobone@gmail.com>
2015-02-28 07:47:28 +0800

    Include symfony/yaml constraint.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build/travis.sh
+++ build/travis.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
-
 if [[ "$TRAVIS_PHP_VERSION" == *5.6* ]]; then
-    composer require "satooshi/php-coveralls:~0.6.1" "symfony/config=~2.0" --prefer-source --no-interaction --dev;
+    composer require "satooshi/php-coveralls:~0.6.1" "symfony/config=~2.0" "symfony/yaml=~2.0" --prefer-source --no-interaction --dev;
 else
     composer install --prefer-source --no-interaction
 fi
