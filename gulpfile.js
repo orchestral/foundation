f9663910bd9e521426b4b121763ed0852be8c373
crynobone <crynobone@gmail.com>
2015-03-10 08:29:57 +0800

    Convert to manage Twitter Bootstrap using bower.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,65 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    coffee = require('gulp-coffee'),
-    csso = require('gulp-minify-css'),
-    less = require('gulp-less'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify');
+  gutil = require('gulp-util'),
+  coffee = require('gulp-coffee'),
+  csso = require('gulp-minify-css'),
+  less = require('gulp-less'),
+  rename = require('gulp-rename'),
+  uglify = require('gulp-uglify'),
+  underscore = require('underscore'),
+  dir;
+
+dir = {
+  asset: 'resources/assets',
+  bower: 'resources/components',
+  web: 'resources/public'
+};
 
 // Less
 gulp.task('css', function () {
-    return gulp.src('resources/assets/less/orchestra.less')
-        .pipe(less())
-        .pipe(csso())
-        .pipe(gulp.dest('resources/public/css'));
+  return gulp.src(dir.asset+'/less/orchestra.less')
+    .pipe(less())
+    .pipe(csso())
+    .pipe(gulp.dest('resources/public/css'));
 });
 
 // Coffee
 gulp.task('js', function () {
-    return gulp.src('resources/assets/coffee/orchestra.coffee')
-        .pipe(coffee().on('error', gutil.log))
-        .pipe(gulp.dest('resources/public/js'));
+  return gulp.src(dir.asset+'/coffee/orchestra.coffee')
+    .pipe(coffee().on('error', gutil.log))
+    .pipe(gulp.dest(dir.web+'/js'));
 });
 
 // Minify JavaScript
 gulp.task('uglify', function () {
-    var options = {
-        outSourceMaps: false,
-        output: {
-            max_line_len: 150
-        }
-    };
+  var options = {
+    outSourceMaps: false,
+    output: {
+      max_line_len: 150
+    }
+  };
 
-    return gulp.src('resources/public/js/orchestra.js')
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify(options))
-        .pipe(gulp.dest('resources/public/js'))
+  return gulp.src(dir.web+'/js/orchestra.js')
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify(options))
+    .pipe(gulp.dest(dir.web+'/js'))
+});
+
+gulp.task('copy', function () {
+  var copy = [
+    [dir.bower+'/bootstrap/dist/**/*', dir.web+'/vendor/bootstrap']
+  ];
+
+  underscore.each(copy, function (file) {
+    gulp.src(file[0]).pipe(gulp.dest(file[1]));
+  });
 });
 
 // Add file watch
 gulp.task('watch', function () {
-    gulp.watch('resources/assets/less/orchestra.less', ['css']);
-    gulp.watch('resources/assets/coffee/orchestra.coffee', ['js']);
-    gulp.watch('resources/public/css/orchestra.js', ['uglify']);
+  gulp.watch(dir.asset+'/less/orchestra.less', ['css']);
+  gulp.watch(dir.asset+'/coffee/orchestra.coffee', ['js']);
+  gulp.watch(dir.web+'/css/orchestra.js', ['uglify']);
 });
 
 // Default task.
-gulp.task('default', ['css', 'js', 'uglify', 'watch']);
-
-// Run default without watch.
-gulp.task('run', ['css', 'js', 'uglify']);
+gulp.task('default', ['css', 'copy', 'js', 'uglify']);
