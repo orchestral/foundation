d92d1e51efe4bf2d2ce17d1524f5f8d4560469d3
crynobone <crynobone@gmail.com>
2015-06-21 22:18:44 +0800

    use PassThroughTrait.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Http/Middleware/VerifyCsrfToken.php
+++ src/Http/Middleware/VerifyCsrfToken.php
@@ -1,17 +1,13 @@
 <?php namespace Orchestra\Foundation\Http\Middleware;
 
+use Orchestra\Http\Traits\PassThroughTrait;
 use Illuminate\Contracts\Encryption\Encrypter;
 use Orchestra\Contracts\Foundation\Foundation;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
 {
-    /**
-     * The application implementation.
-     *
-     * @var \Orchestra\Contracts\Foundation\Foundation
-     */
-    protected $foundation;
+    use PassThroughTrait;
 
     /**
      * Create a new middleware instance.
@@ -24,23 +20,5 @@ class VerifyCsrfToken extends BaseVerifier
         $this->foundation = $foundation;
 
         parent::__construct($encrypter);
-    }
-
-    /**
-     * Determine if the request has a URI that should pass through CSRF verification.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     *
-     * @return bool
-     */
-    protected function shouldPassThrough($request)
-    {
-        foreach ($this->except as $except) {
-            if ($this->foundation->is($except)) {
-                return true;
-            }
-        }
-
-        return false;
     }
 }
