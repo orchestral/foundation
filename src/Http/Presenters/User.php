ae372d01da49ce099d74ea9f09c93de7af60005e
Steve Bauman <steven_bauman@outlook.com>
2015-08-08 13:34:28 -0400

    [3.1] Allow only the fullname and email attribute to be sortable

--- src/Http/Presenters/User.php
+++ src/Http/Presenters/User.php
@@ -43,7 +43,7 @@ class User extends Presenter
         return $this->table->of('orchestra.users', function (TableGrid $table) use ($model) {
             // attach Model and set pagination option to true
             $table->with($model);
-            $table->sortable();
+            $table->sortable(['only' => 'fullname', 'email']);
 
             $table->layout('orchestra/foundation::components.table');
 
