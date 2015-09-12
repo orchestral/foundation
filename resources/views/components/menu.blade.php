c37cfc4d06725088c9da0ee2e015f63e0ab6fe7a
crynobone <crynobone@gmail.com>
2015-09-13 02:37:33 +0800

    Reduce security risk.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -3,20 +3,20 @@
 	@foreach($menu as $item)
 		#{{ $parent }}
 		@if(1 > count($item->get('childs')))
-			<li data-menu="{!! $parent !!}">
+			<li data-menu="{{ $parent }}">
 				#{{ $active = $item->active() ? $parent : $active }}
-				<a href="{!! $item->get('link') !!}">
+				<a href="{{ $item->get('link') }}">
 					{!! $item->get('title') !!}
 				</a>
 			</li>
 		@else
-			<li data-menu="{!! $parent !!}" class="dropdown">
+			<li data-menu="{{ $parent }}" class="dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">{!! $item->get('title') !!}</a>
 				<ul class="dropdown-menu">
 					@if($item->hasLink())
-					<li data-menu="{!! $parent !!}">
+					<li data-menu="{{ $parent }}">
 						#{{ $active = $item->active() ? $parent : $active }}
-						<a href="{!! $item->get('link') !!}">
+						<a href="{{ $item->get('link') }}">
 							{!! $item->get('title') !!}
 						</a>
 					</li>
@@ -29,7 +29,7 @@
 								['class' => $child->active() ? 'active' : ''],
 								['class' => ! empty($grands) ? 'dropdown-submenu' : 'normal']
 							)) }}>
-							<a href="{!! $child->get('link') !!}">
+							<a href="{{ $child->get('link') }}">
 								{!! $child->get('title') !!}
 							</a>
 							@if(! empty($child->get('childs')))
@@ -37,7 +37,7 @@
 								@foreach($child->get('childs') as $grand)
 								#{{ $active = $grand->active() ? $parent : $active }}
 								<li{{ HTML::attributes(['class' => $grand->active() ? 'active' : '']) }}>
-									<a href="{!! $grand->get('link') !!}">
+									<a href="{{ $grand->get('link') }}">
 										{!! $grand->get('title') !!}
 									</a>
 								</li>
