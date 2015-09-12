edab098888065d3add497a97d51180a735a03984
crynobone <crynobone@gmail.com>
2015-09-13 02:32:51 +0800

    Update parent on relevant list.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,19 +1,20 @@
 #{{ $active = null }}
 <ul class="nav navbar-nav" role="menu">
 	@foreach($menu as $item)
+		#{{ $parent }}
 		@if(1 > count($item->get('childs')))
-			<li data-menu="{!! $parent = $item->get('id') !!}">
+			<li data-menu="{!! $parent !!}">
 				#{{ $active = $item->active() ? $parent : $active }}
 				<a href="{!! $item->get('link') !!}">
 					{!! $item->get('title') !!}
 				</a>
 			</li>
 		@else
-			<li class="dropdown">
+			<li data-menu="{!! $parent !!}" class="dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">{!! $item->get('title') !!}</a>
 				<ul class="dropdown-menu">
 					@if($item->hasLink())
-					<li data-menu="{!! $parent = $item->get('id') !!}">
+					<li data-menu="{!! $parent !!}">
 						#{{ $active = $item->active() ? $parent : $active }}
 						<a href="{!! $item->get('link') !!}">
 							{!! $item->get('title') !!}
