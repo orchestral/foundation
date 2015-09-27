f7c2d230424cab8b0d4c4f3d88302b0463633a27
Steve Bauman <steven_bauman@outlook.com>
2015-09-27 08:29:07 +0800

    Add caret to signify dropdown menu
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/components/usernav.blade.php
+++ resources/views/components/usernav.blade.php
@@ -3,10 +3,14 @@
 @if(get_meta('navigation::usernav', true))
 <ul class="nav navbar-nav navbar-right">
 	<li class="dropdown" id="user-menu">
-		<a href="#user-menu" rel="user-menu" class="dropdown-toggle" data-toggle="dropdown">
-			<i class="icon-user"></i> {{ ! is_null($user) ? $user->fullname : trans('orchestra/foundation::title.login') }}
+		@if(is_null($user))
+		<a href="{!! handles('orchestra::login') !!}" rel="user-menu">
+			{{ trans('orchestra/foundation::title.login') }}
 		</a>
-		@unless(is_null($user))
+		@else
+		<a href="#user-menu" rel="user-menu" class="dropdown-toggle" data-toggle="dropdown">
+			<i class="icon-user"></i> {{ $user->fullname }} <span class="caret"></span>
+		</a>
 		<ul class="dropdown-menu">
 			<li>
 				<a href="{!! handles('orchestra::account') !!}">
@@ -25,7 +29,7 @@
 				</a>
 			</li>
 		</ul>
-		@endunless
+		@endif
 	</li>
 </ul>
 @endif
