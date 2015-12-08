0b35ee1d57f9e89497be5cf0ab42a7ea4e0c9fd9
crynobone <crynobone@gmail.com>
2015-12-08 23:08:31 +0800

    Use Orchestra\Http\Middleware\RequireCsrfToken.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Http/Middleware/RequireCsrfToken.php
+++ src/Http/Middleware/RequireCsrfToken.php
@@ -1,61 +1,8 @@
 <?php namespace Orchestra\Foundation\Http\Middleware;
 
-use Closure;
-use Illuminate\Support\Str;
-use Illuminate\Contracts\Encryption\Encrypter;
-use Illuminate\Session\TokenMismatchException;
+use Orchestra\Http\Middleware\RequireCsrfToken as BaseVerifier;
 
-class RequireCsrfToken
+class RequireCsrfToken extends BaseVerifier
 {
-    /**
-     * The encrypter implementation.
-     *
-     * @var \Illuminate\Contracts\Encryption\Encrypter
-     */
-    protected $encrypter;
-
-    /**
-     * Create a new filter instance.
-     *
-     * @param  \Illuminate\Contracts\Encryption\Encrypter  $encrypter
-     */
-    public function __construct(Encrypter $encrypter)
-    {
-        $this->encrypter = $encrypter;
-    }
-
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     *
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        if (! $this->tokensMatch($request)) {
-            throw new TokenMismatchException();
-        }
-
-        return $next($request);
-    }
-
-    /**
-     * Determine if the session and input CSRF tokens match.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     *
-     * @return bool
-     */
-    protected function tokensMatch($request)
-    {
-        $token = $request->input('_token') ?: $request->header('X-CSRF-TOKEN');
-
-        if (! $token && $header = $request->header('X-XSRF-TOKEN')) {
-            $token = $this->encrypter->decrypt($header);
-        }
-
-        return Str::equals($request->session()->token(), $token);
-    }
+    //
 }
