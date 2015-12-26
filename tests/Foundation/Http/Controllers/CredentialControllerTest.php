127ae9987cfe90ab26fcd18c40b3e1ed6fdfec96
crynobone <crynobone@gmail.com>
2015-12-26 20:09:35 +0800

    Small cleanup.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/Foundation/Http/Controllers/CredentialControllerTest.php
+++ tests/Foundation/Http/Controllers/CredentialControllerTest.php
@@ -185,7 +185,6 @@ class CredentialControllerTest extends TestCase
         $this->app->instance('Orchestra\Foundation\Processor\DeauthenticateUser', $deauthenticate);
         $this->app->instance('Orchestra\Contracts\Auth\Command\ThrottlesLogins', $throttles);
 
-
         return [$authenticate, $deauthenticate];
     }
 }
