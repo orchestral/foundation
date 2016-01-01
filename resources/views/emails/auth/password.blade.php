772364481dea5b0179cee89f044decf6258c13f2
crynobone <crynobone@gmail.com>
2016-01-01 18:49:44 +0800

    Edit email templates.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/emails/auth/password.blade.php
+++ resources/views/emails/auth/password.blade.php
@@ -7,7 +7,7 @@
         <h2>Password Reset</h2>
 
         <p>
-            To reset your password, complete this form: {{ handles("orchestra::forgot/reset/{$token}?email=".urlencode($user->getEmailForPasswordReset())) }}.<br/>
+            To reset your password, complete this form: {{ handles("orchestra::forgot/reset/{$token}?email={$email}") }}.<br/>
             This link will expire in {{ config('auth.passwords.'.config('auth.defaults.passwords', 'users').'.expire', 60) }} minutes.
         </p>
     </body>
