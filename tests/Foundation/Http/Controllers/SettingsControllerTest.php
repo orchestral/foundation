8c493c6f60d81369a82ed3defcdbe4e63b666bc7
crynobone <crynobone@gmail.com>
2016-02-12 16:58:39 +0800

    Fixes tests.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/Foundation/Http/Controllers/SettingsControllerTest.php
+++ tests/Foundation/Http/Controllers/SettingsControllerTest.php
@@ -56,7 +56,8 @@ class SettingsControllerTest extends TestCase
         $memory = m::mock('\Orchestra\Contracts\Memory\Provider');
         list($presenter, ) = $this->bindDependencies();
 
-        $memory->shouldReceive('get')->times(16)->andReturn('');
+        $memory->shouldReceive('get')->times(13)->andReturn('')
+            ->shouldReceive('secureGet')->times(3)->andReturn('');
         $presenter->shouldReceive('form')->once()->andReturn('edit.settings');
 
         $this->app->instance('Orchestra\Contracts\Memory\Provider', $memory);
@@ -97,9 +98,10 @@ class SettingsControllerTest extends TestCase
         $memory = m::mock('\Orchestra\Contracts\Memory\Provider');
         list(, $validator) = $this->bindDependencies();
 
-        $memory->shouldReceive('put')->times(16)->andReturnNull()
-            ->shouldReceive('get')->once()->with('email.password')->andReturn('foo')
-            ->shouldReceive('get')->once()->with('email.secret')->andReturn('foo');
+        $memory->shouldReceive('put')->times(13)->andReturnNull()
+            ->shouldReceive('securePut')->times(3)->andReturnNull()
+            ->shouldReceive('secureGet')->once()->with('email.password')->andReturn('foo')
+            ->shouldReceive('secureGet')->once()->with('email.secret')->andReturn('foo');
         $validator->shouldReceive('on')->once()->with('smtp')->andReturn($validator)
             ->shouldReceive('with')->once()->with($input)->andReturn($validator)
             ->shouldReceive('fails')->once()->andReturn(false);
