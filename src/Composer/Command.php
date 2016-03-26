0a10529f3bddb9a234e942c797bb21c6c22fa09a
crynobone <crynobone@gmail.com>
2016-03-26 12:21:26 +0800

    Add composer post install/update for Orchestra Platform.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Composer/Command.php
@@ -0,0 +1,29 @@
+<?php namespace Orchestra\Foundation\Composer;
+
+use Orchestra\Foundation\Application;
+use Illuminate\Foundation\ComposerScripts;
+
+class Command extends ComposerScripts
+{
+    /**
+     * Clear the cached Laravel bootstrapping files.
+     *
+     * @return void
+     */
+    private static function clearCompiled()
+    {
+        $laravel = new Application(getcwd());
+
+        if (file_exists($compiledPath = $laravel->getCachedCompilePath())) {
+            @unlink($compiledPath);
+        }
+
+        if (file_exists($servicesPath = $laravel->getCachedServicesPath())) {
+            @unlink($servicesPath);
+        }
+
+        if (file_exists($extensionServicesPath = $laravel->getCachedExtensionServicesPath())) {
+            @unlink($extensionServicesPath);
+        }
+    }
+}
