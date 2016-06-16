0027995abe334784e1e8a3297ca944209c3430b2
crynobone <crynobone@gmail.com>
2016-06-16 18:56:39 +0800

    Working on dash widget.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/dashboard/_welcome.php
+++ resources/views/dashboard/_welcome.php
@@ -4,9 +4,3 @@
 		If you need help getting started, check out our <a href="http://orchestraplatform.com/docs/3.2" target="_blank">documentation</a>.
 	</p>
 </div>
-
-<div class="row">
-    <div class="col-xs-12 col-sm-3">
-        <dash value="994300" title="Posts"></dash>
-    </div>
-</div>
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,11 +1,17 @@
 @extends('orchestra/foundation::layouts.app')
 
 @section('content')
+<div class="row">
+  <div class="col-xs-12 col-sm-3" v-for="item in dash">
+    <dash :value="item.value" :title="item.title" :color="item.color" :icon="item.icon" :prefix="item.prefix" :suffix="item.suffix"></dash>
+  </div>
+</div>
 @include('orchestra/foundation::dashboard._welcome')
 @stop
 
 @push('orchestra.footer')
 <script>
   var app = Platform.make('app').nav('home').$mount('body')
+  app.$set('dash', {!! app('orchestra.widget')->make('dash.orchestra')->toJson() !!})
 </script>
 @endpush
