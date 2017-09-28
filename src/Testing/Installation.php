2223beb88332fef79924b9936f2e1fecf6c77c96
Mior Muhammad Zaki <crynobone@gmail.com>
2017-09-28 10:02:57 +0800

    Add installation helper.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Testing/Installation.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Orchestra\Foundation\Testing;
+
+trait Installation
+{
+    /**
+     * The administrator user.
+     *
+     * @var \Orchestra\Foundation\Auth\User|null
+     */
+    protected $adminUser;
+
+    /**
+     * Make Orchestra Platform installer.
+     *
+     * @return \Orchestra\Installation\Installation
+     */
+    protected function makeInstaller()
+    {
+        $installer = new Installation($this->app);
+
+        $installer->bootInstallerFilesForTesting();
+        $installer->migrate();
+
+        $this->beforeApplicationDestroyed(function () {
+            $this->artisan('migrate:rollback');
+        });
+
+        return $installer;
+    }
+
+    /**
+     * Install Orchestra Platform and get the administrator user.
+     *
+     * @param  \Orchestra\Contracts\Installation\Installation|null  $installer
+     * @param  array  $config
+     *
+     * @return \Orchestra\Foundation\Auth\User
+     */
+    protected function install(InstallationContract $installer = null, array $config = [])
+    {
+        if (is_null($installer)) {
+            $installer = $this->makeInstaller();
+        }
+
+        $this->artisan('migrate');
+
+        $this->adminUser = $this->createAdminUser();
+
+        $installer->create($this->adminUser, [
+            'site_name' => Arr::get($config, 'name', 'Orchestra Platform'),
+            'email' => Arr::get($config, 'email', 'hello@orchestraplatform.com'),
+        ]);
+
+        $this->app['orchestra.installed'] = true;
+
+        $this->beforeApplicationDestroyed(function () {
+            $this->app['orchestra.installed'] = false;
+
+            $this->artisan('migrate:rollback');
+        });
+
+        return $this->adminUser;
+    }
+
+    /**
+     * Create admin user.
+     *
+     * @return \Orchestra\Foundation\Auth\User
+     */
+    abstract protected function createAdminUser();
+}
