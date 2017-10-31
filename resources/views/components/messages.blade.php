6fae9822654a4efb7d4b7976ac7d6088116481bd
Mior Muhammad Zaki <crynobone@gmail.com>
2017-10-31 09:29:34 +0800

    Use @php helper.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- resources/views/components/messages.blade.php
+++ resources/views/components/messages.blade.php
@@ -1,7 +1,12 @@
-#{{ $message = app('orchestra.messages')->retrieve() }}
+@php
+$message = app('orchestra.messages')->retrieve();
+$content = $content ?? '<div class="alert alert-:key">:message <button class="close" data-dismiss="alert">×</button></div>';
+@endphp
 
 @if($message instanceof Orchestra\Messages\MessageBag)
-  #{{ $message->setFormat('<div class="alert alert-:key">:message <button class="close" data-dismiss="alert">×</button></div>') }}
+  @php
+  $message->setFormat($content);
+  @endphp
 
   @foreach(['error', 'info', 'success'] as $key)
   @if($message->has($key))
