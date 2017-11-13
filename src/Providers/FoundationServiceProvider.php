5b2614cdf2f972cb167c4f5aefc76b08c381763d
Mior Muhammad Zaki <crynobone@gmail.com>
2017-11-13 17:26:19 +0800

    Add missing boot authen provider.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Providers/FoundationServiceProvider.php
+++ src/Providers/FoundationServiceProvider.php
@@ -5,6 +5,7 @@ namespace Orchestra\Foundation\Providers;
 use Laravie\Authen\Authen;
 use Orchestra\Foundation\Meta;
 use Orchestra\Foundation\Foundation;
+use Laravie\Authen\BootAuthenProvider;
 use Orchestra\Foundation\RouteResolver;
 use Illuminate\Contracts\Foundation\Application;
 use Orchestra\Support\Providers\ServiceProvider;
@@ -14,7 +15,7 @@ use Orchestra\Foundation\Auth\Throttle\Basic as BasicThrottle;
 
 class FoundationServiceProvider extends ServiceProvider
 {
-    use AliasesProvider;
+    use AliasesProvider, BootAuthenProvider;
 
     /**
      * List of core aliases.
@@ -134,6 +135,8 @@ class FoundationServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->bootAuthenProvider();
+
         Authen::setIdentifierName('username');
 
         $path = realpath(__DIR__.'/../../');
