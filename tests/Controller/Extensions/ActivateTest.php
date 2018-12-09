4dfd0e59b22247fcd1915c73693f63104eeb7997
Mior Muhammad Zaki <crynobone@gmail.com>
2018-12-09 17:23:05 +0800

    Remove utlizing publisher ftp.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Controller/Extensions/ActivateTest.php
+++ tests/Controller/Extensions/ActivateTest.php
@@ -37,26 +37,4 @@ class ActivateTest extends TestCase
             ->seePageIs('admin/extensions')
             ->seeText('Extension acme/cms activated');
     }
-
-    /** @test */
-    public function it_can_activate_extension_while_requires_asset_publishing()
-    {
-        $this->instance('orchestra.publisher.ftp', $client = m::mock('\Orchestra\Contracts\Publisher\Uploader'));
-        $client->shouldReceive('connected')->once()->andReturn(false);
-
-        $this->instance(Activator::class, $activator = m::mock(Activator::class.'[execute]', [
-            $this->app->make(\Orchestra\Contracts\Extension\Factory::class),
-        ]))->shouldAllowMockingProtectedMethods();
-
-        $activator->shouldReceive('execute')
-            ->with(
-                m::type('\Orchestra\Contracts\Extension\Listener\Activator'), 'activation', m::any(), m::type('\Closure')
-            )->andReturnUsing(function ($listener, $type, $extension) {
-                return $listener->activationHasFailed($extension, []);
-            });
-
-        $this->actingAs($this->adminUser)
-            ->makeRequest('POST', 'admin/extensions/acme/cms/activate')
-            ->seePageIs('admin/publisher/ftp');
-    }
 }
