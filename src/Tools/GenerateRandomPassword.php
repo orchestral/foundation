bf62779448e1e5b2654df2374d03b0c11722dbf2
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-25 09:09:10 +0800

    Add ability to set random password.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Tools/GenerateRandomPassword.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orchestra\Foundation\Tools;
+
+use Illuminate\Support\Str;
+
+class GenerateRandomPassword
+{
+    /**
+     * Generate random password.
+     *
+     * @param  int $length
+     * @return string
+     */
+    public function __invoke(int $length = 6): string
+    {
+        return Str::random($length);
+    }
+}
