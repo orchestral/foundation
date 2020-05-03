9ce7377a71e276ebadc6ebc0010799a83fe9a911
Mior Muhammad Zaki <crynobone@gmail.com>
2020-05-03 21:28:21 +0800

    Add mail::panel
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- resources/views/notifications/emails/welcome.blade.php
+++ resources/views/notifications/emails/welcome.blade.php
@@ -3,10 +3,12 @@
 
 {{ trans('orchestra/foundation::email.register.message.intro') }}
 
+@component('mail::panel')
 {{ trans('orchestra/foundation::email.register.message.email', \compact('email')) }}<br>
 @unless(is_null($password))
 {{ trans('orchestra/foundation::email.register.message.password', \compact('password')) }}
 @endunless
+@endcomponent
 
 Thanks,<br>
 {{ memorize('site.name') }}
